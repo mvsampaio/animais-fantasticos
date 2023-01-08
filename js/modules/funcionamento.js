638d314d3f6ee3050448d0bb3467f2f3c02a80c4
Marcus Sampaio <sampaiosoftware@gmail.com>
2023-01-08 16:28:37 -0300

    adicionado em contato a informação de horário de funcionamento

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,19 @@
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+
+  const dataAgora = new Date();
+  const diaAtual = dataAgora.getDay();
+  const horarioAtual = dataAgora.getHours();
+  const semanaAberto = diasSemana.indexOf(diaAtual) !== -1;
+
+  let horarioAberto =
+    horarioAtual >= horarioSemana[0] && horarioAtual < horarioSemana[1];
+
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto");
+  } else if (funcionamento.classList.contains("aberto")) {
+    funcionamento.classList.remove("aberto");
+  }
+}
