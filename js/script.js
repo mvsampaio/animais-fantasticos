58a799abd894e06dbc7c8d48473fef97720c76d9
Marcus Sampaio <sampaiosoftware@gmail.com>
2023-02-01 17:17:29 -0300

    adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import MenuMobile from "./modules/menu-mobile.js";
 import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
+import SlideNav from "./modules/slide.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -47,3 +48,6 @@ functionamento.init();
 fetchAnimais("./animaisapi.json", ".numeros-grid");
 
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
+
+const slide = new SlideNav(".slide", ".wrapper");
+slide.init().addControl(".custom-controls");
